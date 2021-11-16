403fddb7b5d45e48d23e309c92825454e56f7a77
Nikd0 <47473562+Zettovec@users.noreply.github.com>
2021-11-16 11:59:30 +0100

    Fix ArrayIndexOutOfBonds, Add command help

--- src/main/java/ga/nikd0/ServerJumpSK/commands/CommandServerJump.java
+++ src/main/java/ga/nikd0/ServerJumpSK/commands/CommandServerJump.java
@@ -21,7 +21,9 @@ public class CommandServerJump implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof ConsoleCommandSender) {
-            if (args[0].equalsIgnoreCase("configreload") && args.length == 1) {
+            if (args.length == 0) {
+                Bukkit.getConsoleSender().sendMessage("§9[§bServerJumpSK§9] §bUse §f/sjsk help§b to get a list of possible commands.");
+            } else if (args[0].equalsIgnoreCase("configreload") && args.length == 1) {
                 reloadPluginConfig();
             } else if (args[0].equalsIgnoreCase("jump")) {
                 if (args.length != 3){
@@ -31,6 +33,8 @@ public class CommandServerJump implements CommandExecutor {
 
                 jumpPlayer(args[1], args[2]);
 
+            } else if (args[0].equalsIgnoreCase("help")) {
+                showCommandHelp();
             } else {
                 Bukkit.getConsoleSender().sendMessage("§9[§bServerJumpSK§9] §cUnrecognized or wrongly used command.");
             }
@@ -41,12 +45,16 @@ public class CommandServerJump implements CommandExecutor {
             if (!canUseCommand(player)){
                 return true;
             }
-            
-            if (args[0].equalsIgnoreCase("configreload") && args.length == 1){
+
+            if (args.length == 0) {
+                player.sendMessage("§9[§bServerJumpSK§9] §bUse §f/sjsk help§b to get a list of possible commands.");
+            } else if (args[0].equalsIgnoreCase("configreload") && args.length == 1){
                 reloadPluginConfig(player);
             } else if (args[0].equalsIgnoreCase("jump")) {
                 player.sendMessage("§9[§bServerJumpSK§9] §cThis command may only be executed from console.");
 
+            } else if (args[0].equalsIgnoreCase("help")) {
+                showCommandHelp(player);
             } else {
                 player.sendMessage("§9[§bServerJumpSK§9] §cUnrecognized or wrongly used command.");
             }
@@ -84,5 +92,19 @@ public class CommandServerJump implements CommandExecutor {
         } else {
             Bukkit.getConsoleSender().sendMessage("§9[§bServerJumpSK§9] §cSpecified player is not online!");
         }
+    }
+
+    private void showCommandHelp(){
+        Bukkit.getConsoleSender().sendMessage("§1===== §9[§bServerJumpSK§9] §1=====");
+        Bukkit.getConsoleSender().sendMessage("§f/sjsk configreload §b- Reload plugin config");
+        Bukkit.getConsoleSender().sendMessage("§f/sjsk jump <player> <server> §b- Jump player to another server (console only)");
+        Bukkit.getConsoleSender().sendMessage("§1==================================");
+    }
+
+    private void showCommandHelp(Player player){
+        player.sendMessage("§1===== §9[§bServerJumpSK§9] §1=====");
+        player.sendMessage("§f/sjsk configreload §b- Reload plugin config");
+        player.sendMessage("§f/sjsk jump <player> <server> §b- Jump player to another server (console only)");
+        player.sendMessage("§1==================================");
     }
 }
